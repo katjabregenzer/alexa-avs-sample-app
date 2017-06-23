aaa1eb29d6bddcd69086adaa164e5c2f57b435da
kbregenzer <katja.bregenzer@bridging-it.de>
2017-06-23 16:13:16 +0200

    changed request method

--- samples/javaclient/src/main/java/com/amazon/alexa/avs/alexaOnMirror/MirrorConnectionService.java
+++ samples/javaclient/src/main/java/com/amazon/alexa/avs/alexaOnMirror/MirrorConnectionService.java
@@ -7,22 +7,25 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class MirrorConnectionService {
 
     private final String URL = "http://localhost:8080/refreshCardWithPayload";
+    private static final Logger log = LoggerFactory.getLogger(MirrorConnectionService.class);
 
     public void refreshCard(RenderTemplate payload) {
         HttpClient client = HttpClientBuilder.create().build();
         HttpPost post = new HttpPost(URL);
         post.setHeader("X-GWT-Permutation", "3DE824138FE65400740EC1816A73CACC");
         post.setHeader("Content-Type", "text/x-gwt-rpc; charset=UTF-8");
-
+        log.debug(payload.getTextField());
         try {
-            StringEntity se = new StringEntity( payload.getTextField());
-            post.setEntity(se);
+           // StringEntity se = new StringEntity( payload.getTextField());
+            //post.setEntity(se);
             HttpResponse response = client.execute(post);
 
             // Print out the response message

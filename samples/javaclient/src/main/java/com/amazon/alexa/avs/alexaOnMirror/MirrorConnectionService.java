f7d35de9c85a968362cb0f9d98f8c90997f8faf6
Benjamin Gniza <benjamin.gniza@bridging-it.de>
2018-03-27 10:58:33 +0200

    Change MirrorConnectionService.java to use commons http client
    MirrorConnectionService.java was a bit unstable at our installation
    especially with weather cards. Changing to commons http client fixed the
    issue.

--- samples/javaclient/src/main/java/com/amazon/alexa/avs/alexaOnMirror/MirrorConnectionService.java
+++ samples/javaclient/src/main/java/com/amazon/alexa/avs/alexaOnMirror/MirrorConnectionService.java
@@ -1,29 +1,34 @@
 package com.amazon.alexa.avs.alexaOnMirror;
 
-import org.eclipse.jetty.client.HttpClient;
-import org.eclipse.jetty.client.api.ContentResponse;
-import org.eclipse.jetty.http.HttpMethod;
-import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MirrorConnectionService {
 
-    private final String URL = "http://localhost:8080/refreshCardWithPayload";
     private static final Logger log = LoggerFactory.getLogger(MirrorConnectionService.class);
+    private final        String URL = "http://localhost:8080/refreshCardWithPayload";
 
     public void refreshCard(String payload) {
         log.debug(payload);
         try {
-            final SslContextFactory sslContextFactory = new SslContextFactory();
-            HttpClient httpClient = new HttpClient(sslContextFactory);
-            httpClient.start();
-            final ContentResponse contentResponse = httpClient.newRequest(URL).method(HttpMethod.POST)
-                    .param("card", payload)
-                    .agent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0")
-                    .send();
-            log.debug(contentResponse.getContentAsString());
-
+            CloseableHttpClient httpclient = HttpClients.createDefault();
+            HttpPost httpPost = new HttpPost(URL);
+            List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+            nvps.add(new BasicNameValuePair("card", payload));
+            httpPost.setEntity(new UrlEncodedFormEntity(nvps, "UTF-8"));
+            CloseableHttpResponse response = httpclient.execute(httpPost);
+            log.debug(response.toString());
+            //System.err.println("Resonse: " + response.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
